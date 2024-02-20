f45b974176125550afef59ebda1c51381e75d352
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2024-02-20 10:52:53 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,27 +1,52 @@
 package test;
 
 public class Auto {
-    static int cantidadCreados;
+    static int cantidadCreados = 0;
     String modelo;
     int precio;
-    Asiento[] asientos ;
+    Asiento[] asientos;
+    String marca;
     Motor motor;
     int registro;
-    String marca;
+
+    public Auto(String modelo, int precio, Asiento[] asientos, String marca, Motor motor, int registro) {
+        this.modelo = modelo;
+        this.precio = precio;
+        this.asientos = asientos;
+        this.marca = marca;
+        this.motor = motor;
+        this.registro = registro;
+    }
 
     public int cantidadAsientos() {
-
+        int asientosContados = 0;
+        for (Asiento asiento : this.asientos) {
+            if (asiento != null) {
+                asientosContados++;
+            }
+        }
+        return asientosContados;
     }
 
-    public String verificarIntegridad(){
-
+    public String verificarIntegridad() {
+        boolean registrosCoinciden = true;
+        int registroMotor = this.motor.getRegistro();
+        if (this.registro != registroMotor) {
+            registrosCoinciden = false;
+        }
+        for (Asiento asiento : this.asientos) {
+            if (asiento != null && asiento.getRegistro() != this.registro) {
+                registrosCoinciden = false;
+                break;
+            }
+        }
+        if (registrosCoinciden) {
+            return "Auto original";
+        } else {
+            return "Las piezas no son originales";
+        }
     }
-
-    
-
-    
 }
-
 
 
 
