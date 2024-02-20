4f1015336d11c2d85130fa505f21cbee57e99334
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2024-02-20 04:28:23 -0500

    Create Auto.java

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,27 @@
+package test;
+
+public class Auto {
+    static int cantidadCreados;
+    String modelo;
+    int precio;
+    Asiento[] asientos ;
+    Motor motor;
+    int registro;
+    String marca;
+
+    public int cantidadAsientos() {
+
+    }
+
+    public String verificarIntegridad(){
+
+    }
+
+    
+
+    
+}
+
+
+
+
