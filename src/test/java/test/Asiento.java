a4d202b5a329f195df45f12d87b9667e8770f5ac
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2024-02-20 04:28:35 -0500

    Create Asiento.java

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Asiento {
+    String color;
+    int precio;
+    int registro;
+    String [] colores = {"rojo", "verde", "amarillo", "negro" , "blanco"};
+
+    public void cambiarColor(String color){
+        
+
+    }
+}
