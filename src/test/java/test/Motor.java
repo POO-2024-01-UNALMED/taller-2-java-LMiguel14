2ed7c45c82987a8a9a3f3eec3c74aa64f2bea106
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2024-02-20 10:53:03 -0500

    Update Motor.java

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,24 +1,29 @@
 package test;
-
 public class Motor {
-    int numeroCilindros;
+    int cilindros;
     String tipo;
     int registro;
 
-    public void cambiarRegistro(int registro){
+    public Motor(int cilindros, String tipo, int registro) {
+        this.cilindros = cilindros;
+        this.tipo = tipo;
         this.registro = registro;
     }
-    public void asignarTipo(String tipo){
-        
-        if ( tipo == "electrico") {
-            this.tipo = tipo;
-        }
-        if (tipo== "gasolina") {
-            this.tipo = tipo;
+
+    public void cambiarRegistro(int numero) {
+        this.registro = numero;
+    }
+
+    public void asignarTipo(String nuevoTipo) {
+        if ("gasolina".equals(nuevoTipo) || "electrico".equals(nuevoTipo)) {
+            this.tipo = nuevoTipo;
         }
     }
-}
 
+    public int getRegistro() {
+        return this.registro;
+    }
+}
 
 
 
