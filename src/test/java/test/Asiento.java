c51165d5ce428ebf50641cca6b96ebb80f73a1af
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2024-02-20 10:52:58 -0500

    Update Asiento.java

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -5,14 +5,23 @@ public class Asiento {
     int precio;
     int registro;
 
+    public Asiento(String color, int precio, int registro) {
+        this.color = color;
+        this.precio = precio;
+        this.registro = registro;
+    }
+
     public void cambiarColor(String nuevoColor) {
         String[] colores = {"verde", "amarillo", "blanco", "negro", "rojo"};
         for (String color : colores) {
-            if (nuevoColor.equals(color)) {
+            if (color.equals(nuevoColor)) {
                 this.color = nuevoColor;
                 break;
             }
-        }   
+        }
     }
-        
+
+    public int getRegistro() {
+        return this.registro;
+    }
 }
